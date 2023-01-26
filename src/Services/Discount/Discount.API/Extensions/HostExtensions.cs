7b7385329313638138d70aee5e3ab0950debfb19
Ravi Gupta <120781848+Ravi-Gupta-HCL@users.noreply.github.com>
2023-01-26 14:08:07 +0530

    Host Extention calss was coded in Catalog.API by mistake now corrected.

---
+++ src/Services/Discount/Discount.API/Extensions/HostExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Discount.API.Extensions
+{
+    public static class HostExtensions
+    {
+        public static IHost MigrateDatabase<TContext>(this IHost host, int? retry = 0)
+        {
+            int retryForAvailability = retry.Value;
+
+            using(var scope = host.Services.CreateScope())
+            {
+
+            }
+        }
+    }
+}
